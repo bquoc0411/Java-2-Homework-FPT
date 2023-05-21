ed388fa2ae981705809376a37b4f21103aae6b49
Binh <hoangquocbinh0411@gmail.com>
2023-05-21 19:19:21 +0700

    feat: add lab 2

---
+++ lab/lab2-java2/src/main/java/App/Lab2App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package App;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab2App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ lab/lab2-java2/src/main/java/Model/User.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Admin
+ */
+public class User implements Serializable{
+    private String username;
+    private String password;
+    private String role;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User() {
+    }
+    
+    
+}
